bb97b25ffe92f3eee5556d17c21e59adbe00377f
Purizz <priskilasupit22@gmail.com>
2024-06-13 08:41:58 +0800

    .

--- image_editor.py
+++ image_editor.py
@@ -1,9 +1,11 @@
 from tkinter import *
 from tkinter import  ttk
 from tkinter import filedialog
+from tkinter.colorchooser import askcolor
 from PIL import ImageTk, Image, ImageEnhance, ImageFilter
 import os
-         
+
+        
 #fungsi mengganti gambar
 def LoadImg():
     global img, ori_img
@@ -36,7 +38,14 @@ def contrast_callback(contrast_pos):
 	outputImage = enhancer.enhance(contrast_pos)
 	displayimage(outputImage)
 
-#fungsi rotasi
+# fungsi scaling image
+def scale_image(scaling_pos):
+    scaling_pos = float(scaling_pos)
+    global img, ori_img
+    img = ori_img.resize((int(ori_img.width * scaling_pos), int(ori_img.height * scaling_pos)))
+    displayimage(img)
+
+# fungsi rotasi
 def rotate_image(direction):
     global img
     if img is not None:
@@ -56,16 +65,98 @@ def mirror():
     img = img.transpose(Image.FLIP_LEFT_RIGHT)
     displayimage(img)
 
-
-
 # fungsi reset image
 def reset_image():
     global img, ori_img
     img = ori_img.copy()  # Kembalikan gambar ke keadaan asli
     displayimage(img)
 
+# fungsi menyimpan gambar
+def save():
+    global img
+    if img:
+        filename = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png"), ("JPEG files", "*.jpg"), ("All files", "*.*")])
+        if filename:
+            img.save(filename)
+
+class Paint(object):
+    DEFAULT_PEN_SIZE = 5.0
+    DEFAULT_COLOR = 'black'
+
+    def __init__(self, root):
+        self.root = root
+
+        self.pen_button = Button(self.root, text='pen', command=self.use_pen)
+        self.pen_button.grid(row=0, column=0)
+
+        self.color_button = Button(self.root, text='color', command=self.choose_color)
+        self.color_button.grid(row=0, column=1)
+
+        self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
+        self.eraser_button.grid(row=0, column=2)
+
+        self.clear_button = Button(self.root, text="Clear Canvas", command=self.clear_canvas)
+        self.clear_button.grid(row=0, column=3)
+
+        self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
+        self.choose_size_button.grid(row=0, column=4)
+
+        self.c = Canvas(self.root, bg='white', width=600, height=600)
+        self.c.grid(row=1, columnspan=6)
+
+        self.setup()
+
+    def setup(self):
+        self.old_x = None
+        self.old_y = None
+        self.line_width = self.choose_size_button.get()
+        self.color = self.DEFAULT_COLOR
+        self.eraser_on = False
+        self.active_button = self.pen_button
+        self.c.bind('<B1-Motion>', self.paint)
+        self.c.bind('<ButtonRelease-1>', self.reset)
+
+    def use_pen(self):
+        self.activate_button(self.pen_button)
+
+    def choose_color(self):
+        self.eraser_on = False
+        self.color = askcolor(color=self.color)[1]
+
+    def use_eraser(self):
+        self.activate_button(self.eraser_button, eraser_mode=True)
+
+    def activate_button(self, some_button, eraser_mode=False):
+        self.active_button.config(relief=RAISED)
+        some_button.config(relief=SUNKEN)
+        self.active_button = some_button
+        self.eraser_on = eraser_mode
+
+    def paint(self, event):
+        self.line_width = self.choose_size_button.get()
+        paint_color = 'white' if self.eraser_on else self.color
+        if self.old_x and self.old_y:
+            self.c.create_line(self.old_x, self.old_y, event.x, event.y,
+                               width=self.line_width, fill=paint_color,
+                               capstyle=ROUND, smooth=TRUE, splinesteps=36)
+        self.old_x = event.x
+        self.old_y = event.y
+
+    def clear_canvas(self):
+        self.c.delete("all")
+
+    def reset(self, event):
+        self.old_x, self.old_y = None, None
+
+
+def open_paint_app():
+    paint_window = Toplevel(root)
+    Paint(paint_window)
+
+if __name__ == '__main__':
+
 # MAIN LOOP 
-root = Tk()
+    root = Tk()
 root.title("Image Editor")
 
 space=(" ")*215
@@ -112,6 +203,11 @@ btnResetImg = Button(frame_t, text='Reset Image', width=15, command=reset_image,
 btnResetImg.configure(font=('poppins', 11, 'bold'), foreground='white')
 btnResetImg.place(x=180, y=15)
 
+# button reset image
+btnResetImg = Button(frame_t, text='Save', width=15, command=save, bg="#939393",activebackground="LIGHTBLUE")
+btnResetImg.configure(font=('poppins', 11, 'bold'), foreground='white')
+btnResetImg.place(x=350, y=15)
+
 #slider untuk brightness
 brightnessSlider = Scale(frame_t, label="Brightness", from_=0, to=2, orient=HORIZONTAL, length=200,
 						resolution=0.1, command=brightness_callback, bg="#616E7C")
@@ -128,6 +224,14 @@ brightnessSlider.set(1)
 brightnessSlider.configure(font=('poppins',11),foreground='white')
 brightnessSlider.place(x=1070,y=1)
 
+# slider untuk scaling image
+scalingSlider = Scale(frame_t, label="Scale", from_=0, to=2, orient=HORIZONTAL, length=200,
+                      resolution=0.1, command=scale_image, bg="#616E7C")
+# initially, scaling position set to 1
+scalingSlider.set(1)
+scalingSlider.configure(font=('poppins', 11), foreground='white')
+scalingSlider.place(x=840, y=1)
+
 #dropdown rotasi
 rotation_options = ["Rotate Left", "Rotate Right", "Rotate 90", "Rotate 180"]
 selected_rotation = StringVar(frame_l)
@@ -140,6 +244,11 @@ rotation_menu.pack(padx=10, pady=10)
 btnmirror = Button(frame_l, text='Mirror', width=10, command=mirror)
 btnmirror.configure(font=('poppins', 11))
 btnmirror.pack(padx=10, pady=10)
+
+#fitur paint
+open_paint_button = Button(frame_l, text="Paint", width=10, command=open_paint_app)
+open_paint_button.configure(font=('poppins', 11))
+open_paint_button.pack(pady=20)
 
 ori_img = None
 
